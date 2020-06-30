c16aa265625175d42ddd71bbdbee2fc705c07ccf
Tom Perry <tombperry0@gmail.com>
2020-06-30 22:12:57 +0100

    All forces applied - Not happy with performance so will redo loops

--- 2D Flocking Game/Assets/Scripts/Flocker.cs
+++ 2D Flocking Game/Assets/Scripts/Flocker.cs
@@ -5,28 +5,74 @@ using UnityEngine;
 public class Flocker : MonoBehaviour
 {
     // Referencing the rigid body to which the script is attatched
-    public Rigidbody rb;
+    public Rigidbody2D rb;
 
-    public float repelStrength = 10f;
-    public float repelDistance = 15f;
+    public float attractStrength = 1f;
+    public float repelStrength = 1f;
+    public float repelDistance = 10f;
+    public float allignDistance = 15f;
+
+    public float allignStrength = 1f;
+
+    public float maxSpeed = 1f;
+
+    public  Vector2 localVeloicty;
+
+    public bool variablesSet = false;
+    
+
+    
 
     // Making a list of all other Boids
     public static List<Flocker> Flockers;
+    // Use this to use the list of N objects made in Generator
+        // go GameObject
+    //go.GetComponent<Scriptname>().listname; 
+
+    void start()
+    {
+        // GameObject GeneratorObject = GameObject.Find("GeneratorObject");
+        // int N = Generator.N;//GeneratorObject.GetComponent<Generator>().N;
+        
+        if (!variablesSet)
+        {
+            attractStrength = attractStrength/Generator.N;
+            Debug.Log(attractStrength);
+
+            variablesSet = true;
+        }
+
+        Debug.Log(variablesSet);
+
+    }
 
     void FixedUpdate()
     {
+        Debug.Log(variablesSet);
         foreach (Flocker flocker in Flockers)
         {
             if (flocker != this)
                 Flock(flocker);
         }
 
+        LimitSpeed(rb);
+    }
+
+    void Update()
+    {
+        Vector2 moveDirection = rb.velocity;
+        if (moveDirection != Vector2.zero) {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg - 90f;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 
     void OnEnable ()
     {
         if (Flockers == null)
+        {
             Flockers = new List<Flocker>();
+        }
 
         Flockers.Add(this);
     }
@@ -41,46 +87,94 @@ public class Flocker : MonoBehaviour
     {
         Attract(objToFlock);
         Repel(objToFlock);
-               
+        Allign(objToFlock);               
     }
 
     // Attracting force
     void Attract(Flocker objToFlock)
     {
-        Rigidbody rbToFlock = objToFlock.rb;
+        Rigidbody2D rbToFlock = objToFlock.rb;
 
         Vector2 direction = rb.position - rbToFlock.position;
-        float distance = direction.magnitude;
+        // float distance = direction.magnitude;
 
-        if (distance == 0f)
-            return;
+        // if (distance < repelDistance/3)
+        //     return;
 
         // Newtonian gravity for POC
-        float forceMagnitude = (rb.mass * rbToFlock.mass) / Mathf.Pow(distance, 2);
-        Vector2 force = direction.normalized * forceMagnitude;
+        float forceMagnitude = 1f;//Mathf.Pow(distance, -2);
+        Vector2 force = direction.normalized * forceMagnitude * attractStrength;
 
         rbToFlock.AddForce(force);
 
     }
+
     // Repelling force
     void Repel(Flocker objToFlock)
     {
-         Rigidbody rbToFlock = objToFlock.rb;
+        Rigidbody2D rbToFlock = objToFlock.rb;
 
         Vector2 direction = rb.position - rbToFlock.position;
-        float distance = direction.magnitude;
 
-        if (distance < repelDistance)
+        // This vector -> scalar can be optimised
+        float distance = magSquare(direction);//.magnitude;
+
+        if (distance < repelDistance*repelDistance)
         {
-            Vector2 force = direction.normalized * repelStrength;
+            Vector2 force = -direction.normalized;
 
-            rbToFlock.AddForce(force);
+            rbToFlock.AddForce(force * repelStrength);
         }
     }
 
     // Alligning force
-    // void Allign(Flocker objToFlock)
-    // {
+    void Allign(Flocker objToFlock)
+    {
         
-    // }
+        Rigidbody2D rbToFlock = objToFlock.GetComponent<Rigidbody2D>();
+
+        foreach (Flocker flocker in Flockers)
+        {
+            if (flocker != this)
+            {
+                Vector2 direction = rb.position - rbToFlock.position;
+
+                // This vector -> scalar can be optimised
+                float distance = magSquare(direction);//.magnitude;
+
+                if (distance < allignDistance*allignDistance)
+                    localVeloicty = localVeloicty + rbToFlock.velocity;
+            }
+        }
+
+        localVeloicty = localVeloicty.normalized;
+        localVeloicty = localVeloicty*maxSpeed;
+
+        Vector2 localVelocityDifference = localVeloicty - rb.velocity;
+
+        Vector2 allignForce = localVelocityDifference*allignStrength;
+        // Debug.Log(allignForce);
+
+        rbToFlock.AddForce(allignForce);
+        
+    }
+
+    void LimitSpeed(Rigidbody2D _rb)
+    {
+        _rb.velocity = _rb.velocity.normalized;
+        _rb.velocity = _rb.velocity*maxSpeed;
+        // Debug.Log(_rb.velocity);
+
+        // if(magSquare(_rb.velocity) > maxSpeed*maxSpeed)
+        // {
+        //     //smoothness of the slowdown is controlled by the 0.99f, 
+        //     //0.5f is less smooth, 0.9999f is more smooth
+        //     _rb.velocity *= 0.95f;
+        // }
+    }
+
+    float magSquare(Vector2 v)
+    {
+        return v.x*v.x + v.y*v.y;
+    }
 }
--- 2D Flocking Game/Assets/Scripts/Generator.cs
+++ 2D Flocking Game/Assets/Scripts/Generator.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
+    public GameObject prefab;
+
+    public static int N = 20;
+    public static float initialSpeed = 0.5f;
+
+    public static GameObject[] Boids = new GameObject[N];
+    private Rigidbody2D _rb;
+
+    public static int size = 20;
+    public static float spacing;
+
     // Start is called before the first frame update
     void Start()
-    {
+    {  
+
+        // Declaring variables
+        float x, y, vx, vy;
         
+        // Generating N GameObjects
+        spacing = 3f; //size/Mathf.Pow(N, 0.5);
+
+        for(int i = 0; i < N; i++)
+        {
+            x = Random.Range(-size/2, size/2);
+            y = Random.Range(-size/2, size/2);
+
+            vx = Random.Range(-initialSpeed, initialSpeed);
+            vy = Random.Range(-initialSpeed, initialSpeed);
+        
+            Generate(i, x, y, vx, vy, 1, false);
+        }
+
+        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Generate(int i, float x, float y, float vx, float vy, float mass, bool frozen)
     {
-        
+        //GameObject[] objects = new GameObject[N];
+        // make the object at (x,y,z)
+        Boids[i] = Instantiate(prefab, new Vector2(x, y), Quaternion.identity);
+
+        // Edit the rigidbody components of the new object
+        _rb = Boids[i].GetComponent<Rigidbody2D>();
+        _rb.velocity = new Vector2(vx, vy);
+        // if (frozen)
+        // {
+        //     _rb.constraints = Rigidbody2DConstraints.FreezePosition;
+        // }
+
+        _rb.mass = mass;
+        //set this as input parameter??
+        _rb.angularDrag = 10f;
     }
 }
